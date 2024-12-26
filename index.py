bdc52a15f07a301e96d17363d5e48894e8bb07f6
LilCharchar <jj453833@gmail.com>
2024-12-26 01:10:45 -0500

    Alpha 1.0

--- index.py
+++ index.py
@@ -1,18 +1,58 @@
+import io
+import sys
 import dearpygui.dearpygui as dpg
 import os
 import yt_dlp
 import tkinter as tk
 from tkinter import filedialog
+import ffmpeg
 
+def log(msg):
+    current_text = dpg.get_value("log")
+    next_text = current_text + msg  + '\n'
+    dpg.set_value("log", next_text) 
 
+class LogRedirect(io.StringIO):
+    def write(self, msg):
+        current_text = dpg.get_value("log")
+        next_text = current_text + msg
+        dpg.set_value("log", next_text)
 
-def actualizar_texto():
-    # Simulando la llegada de nuevos datos
-    new_data = "Nuevo dato recibido...\n"
-    # Añadir los datos al campo de texto
-    current_text = dpg.get_value("text_output")
-    updated_text = current_text + new_data
-    dpg.set_value("text_output", updated_text)
+def descargar():
+
+    URL = dpg.get_value("urls").strip().split('\n')
+
+    PATH = dpg.get_value("path").strip()
+
+    data_type = dpg.get_value("datatype")
+
+    if not PATH:
+        log("Por favor, seleccione una carpeta de descarga.\n")
+        return
+    
+    ydl_opts = {
+    'logtostderr' : True,
+    'outtmpl': os.path.join(PATH, '%(title)s.%(ext)s'),
+    'embedthumbnail': True,
+    'writethumbnail': True
+    
+    }
+
+    if data_type == "Audio":
+        ydl_opts['format'] = 'm4a/bestaudio/best'
+        ydl_opts['postprocessors'] = [{  # Extract audio using ffmpeg
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'm4a'
+        },
+        {
+            'key': 'EmbedThumbnail'
+        }
+        ]
+           
+    for i in range(len(URL)):
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        ydl.download(URL[i])
+           
 
 def examinar():
     root = tk.Tk()
@@ -22,6 +62,13 @@ def examinar():
     if directory_path:
         dpg.set_value("path", directory_path)
     
+def limpiar_urls():
+    dpg.set_value("urls", "")
+
+
+
+sys.stdout = LogRedirect()
+sys.stderr = LogRedirect()
 
 dpg.create_context()
 
@@ -36,9 +83,12 @@ with dpg.window(tag="Primary Window", no_resize=True, width=600, height=600):
 
     dpg.add_text(default_value="Ingrese las URLs de YouTube aquí:")
     with dpg.group(horizontal=True):
-               
-        url = dpg.add_input_text(tag="urls", hint = "URL...", multiline=True, width=400, height= 100)        
-        dpg.add_button(label = "Limpiar Enlaces")
+
+        url = dpg.add_input_text(tag="urls", hint = "URL...", multiline=True, width=400, height= 100)   
+        with dpg.group():   
+            dpg.add_text("")  
+            dpg.add_spacer(height=1)
+            dpg.add_button(label = "Limpiar enlaces", callback=lambda: dpg.set_value("urls", ""))
         
 
     dpg.add_spacer(height=10)  # Separador
@@ -46,23 +96,29 @@ with dpg.window(tag="Primary Window", no_resize=True, width=600, height=600):
     dpg.add_text(default_value="Seleccione la carpeta de descarga:")
     with dpg.group(horizontal=True) as path:
         path = dpg.add_input_text(hint= "Dirección del directorio", tag= "path", width=400)
-        dpg.add_button(label="examinar", callback=examinar)
+        dpg.add_button(label="Examinar", callback=examinar)
     
     dpg.add_spacer(height=10)
     with dpg.group(horizontal=True):
         dpg.add_text(default_value="Tipo de archivo:")
-        dpg.add_combo(items=["Audio", "Video"], default_value="Audio", width= 100 )
+        dpg.add_combo(items=["Audio", "Video"], default_value="Audio", width= 100 , tag= "datatype" )
 
     dpg.add_spacer(height=10)
     # Texto que se actualiza para ver el estado de las descargas
-    dpg.add_input_text(tag="text_output", multiline=True, readonly=True, width=400, height=200, enabled=False)
-
+    with dpg.group(horizontal=True):
+        dpg.add_input_text(tag="log", multiline=True, readonly=True, width=400, height=200, enabled=False, no_horizontal_scroll=False)
+        with dpg.group():
+            dpg.add_text("")  
+            dpg.add_text("") 
+            dpg.add_spacer(height=2) 
+            dpg.add_button(label = "Limpiar logs", callback=lambda: dpg.set_value("log", ""))
     #Boton de descarga de prueba
-    dpg.add_button(label="Descargar", callback=actualizar_texto)
+    dpg.add_button(label="Descargar", callback=descargar)
 
 
 # Parametros de iniciacion de la ventana
 dpg.create_viewport(title='Descargador UwU', width=600, height=600, resizable=False)
+dpg.add_input_text
 
 dpg.setup_dearpygui()
 
@@ -79,3 +135,4 @@ dpg.start_dearpygui()
 #     dpg.render_dearpygui_frame()
 
 dpg.destroy_context()
+
